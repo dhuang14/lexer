3687617a6a42419b441644c6a075c883da388703
David Huang <35814915+dhuang14@users.noreply.github.com>
2019-09-09 21:20:50 -0700

    rough draft

--- readInFiles.cpp
+++ readInFiles.cpp
@@ -1,28 +1,40 @@
+//
+//  main.cpp
+//  readInFile
+//
+//  Created by David Huang on 9/9/19.
+//  Copyright © 2019 David Huang. All rights reserved.
+//
+
 #include <iostream>
 #include <string.h>
 #include <fstream>
+#include <sstream>
 
 using namespace std;
 
-string readInFile(fileName){
-	
-	ifstream file;
-	file.open(fileName);
-	
-	if(!file){
-		cout << "Cannot find file name with " << fileName << endl;
-	}
-	
-	stringstream ss;
-	
-	ss << file.rdbuf();
-	
-	return ss.str();
+string readInFile(string fileName){
+    
+    ifstream file;
+    file.open(fileName);
+    
+    if(!file){
+        cout << "Cannot find file name with " << fileName << endl;
+        exit(1);
+    }
+    
+    stringstream ss;
+    
+    ss << file.rdbuf();
+    
+    file.close();
+    
+    return ss.str();
 }
 
 int main(){
-	
-	cout << readInFile("test.txt") << endl;
-	
-	return 0;
+    
+    cout << readInFile("test.txt") << endl;
+    
+    return 0;
 }
